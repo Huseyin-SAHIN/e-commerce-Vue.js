41c3a6170ae4d3aaac46ffbbc8a69fa572b613be
Hüseyin <sakarya.hsa54@gmail.com>
2023-11-04 00:28:43 +0300

    feat 1: anasayfa tasarım yapıldı

---
+++ src/components/SalesCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row dense>
+    <v-col :cols="flex">
+      <v-card>
+        <v-img :src="src" class="align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="300px" cover>
+          <v-card-title class="text-white" :textContent="price"></v-card-title>
+        </v-img>
+
+        <v-card-actions v-if="buttons">
+          <v-card-title class="text-black" :textContent="title"></v-card-title>
+          <v-spacer></v-spacer>
+          <v-btn v-for="button in buttons" :key="button.id" :size="button.size || 'small'"
+            :color="button.color || 'surface-variant'" :variant="button.text || 'text'" :icon="button.icon || 'mdi-heart'"
+            :price="price || '00.00TL'"></v-btn>
+        </v-card-actions>
+
+        <v-card-actions v-else>
+          <v-card-title class="text-black" :textContent="title"></v-card-title>
+          <v-spacer></v-spacer>
+          <v-btn size="small" color="surface-variant" variant="text" icon="mdi-cart"></v-btn>
+          <v-btn size="small" color="surface-variant" variant="text" icon="mdi-heart"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+
+defineProps({
+  title: String,
+  src: String,
+  flex: Number,
+  cards: Array,
+  price: String,
+  buttons: Array,
+});
+
+
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <h3>Öne Çıkanlar</h3>
+    <v-row>
+      <v-col cols="12" md="6" lg="2" v-for="data in AllData" :key="data.id">
+        <CardOne :title="data.name" :price="data.price" :src="data.image" :flex="12" />
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <h3>Çok Satan Ürünler</h3>
+    <v-row>
+      <v-col cols="12" md="6" lg="2" v-for="data in AllData" :key="data.id">
+        <CardOne :title="data.name" :src="data.image" :flex="12" />
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <h3>Yıldızlı Ürünler</h3>
+    <v-row>
+      <v-col cols="12" md="6" lg="2" v-for="data in AllData" :key="data.id">
+        <CardOne :title="data.name" :src="data.image" :flex="12" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import CardOne from '@/components/SalesCard.vue';
+import AllData from '@/demoData/highlights';
+
+</script>
