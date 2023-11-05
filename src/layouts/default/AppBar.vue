5f5252043e3956b2fe9c727e18e24fcd2d65e524
Hüseyin <sakarya.hsa54@gmail.com>
2023-11-05 17:22:50 +0300

    feat 2: navigation bar eklendi

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,24 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
+            subtitle="sandraadams@gmail.com"></v-list-item>
+        </v-list>
 
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home" title="Anasayfa" value="home" />
+          <v-list-item prepend-icon="mdi-basket" title="Sepetim" value="basket" />
+          <v-list-item prepend-icon="mdi-account" title="Profil" value="profil" />
+          <v-list-item prepend-icon="mdi-star" title="Favoriler" value="star" />
+          <v-list-item prepend-icon="mdi-cog" title="Ayarlar" value="settings" />
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh"></v-main>
+    </v-layout>
+  </v-card>
 </template>
-
-<script setup>
-  //
-</script>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,25 @@
 <template>
   <v-app>
-    <default-bar />
+    <default-bar class="navbar" />
 
     <default-view />
   </v-app>
 </template>
 
 <script setup>
-import DefaultBar from './AppBar.vue'
-import DefaultView from './View.vue'
+  import DefaultBar from './AppBar.vue'
+  import DefaultView from './View.vue'
 </script>
+
+
+<style>
+.navbar {
+  position: fixed;
+  z-index: 1;
+  width: 56px;
+}
+.navbar:hover {
+  width: 256px;
+  transition: 0.3s;
+}
+</style>
